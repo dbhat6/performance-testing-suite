cb02309d1c5f5b8ad16d0aec81d00f1889e82bbb
Deepak Bhat <dbhat@mscripts.com>
2020-07-04 18:20:33 +0530

    Added linter into NodeJS server

--- server/routes/test.js
+++ server/routes/test.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-	setTimeout(() => {
-		res.send('test get');
-	}, 1);
+  setTimeout(() => {
+    res.send('test get');
+  }, 1);
 });
 
 router.post('/test', (req, res) => {
-	res.send('test get');
+  res.send('test get');
 });
 
 module.exports = router;
